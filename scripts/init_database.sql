d3c4f2f89a09ad8d835c233bd42948fbc467dd6a
KhanhLinh0914 <khlinh1412@gmail.com>
2026-01-28 13:47:34 -0500

    Create init_database.sql to set up databases
    This script creates three databases: bronze, silver, and gold, if they do not already exist.

---
+++ scripts/init_database.sql
@@ -0,0 +1,10 @@
+/*
+========================================
+Create Database
+========================================
+SCRIPT PURPOSE: This script creates new 03 database named 'bronze', 'silver', 'gold' after checking if they already exist.
+*/ 
+
+CREATE DATABASE IF NOT EXISTS bronze;
+CREATE DATABASE IF NOT EXISTS silver;
+CREATE DATABASE IF NOT EXISTS gold; 
